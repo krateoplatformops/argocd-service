f4b558b860782ce35f229a58385fb2a3577c36ab
mauro sala <mauro.sala@gmail.com>
2022-10-24 16:43:02 +0200

    feat(*): service-library

--- controllers/read.argocd.controller.js
+++ controllers/read.argocd.controller.js
@@ -1,55 +1,44 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-const uriHelpers = require('../helpers/uri.helpers')
+const uriHelpers = require('../service-library/helpers/uri.helpers')
 const argocdHelpers = require('../helpers/argocd.helpers')
-const stringHelpers = require('../helpers/string.helpers')
+const secretHelpers = require('../service-library/helpers/secret.helpers')
 
 router.get('/:endpoint/:name', async (req, res, next) => {
   try {
-    console.log(stringHelpers.b64toAscii(req.params.endpoint))
-    const endpoint = JSON.parse(stringHelpers.b64toAscii(req.params.endpoint))
+    const endpoint = (await secretHelpers.getEndpoint(req.params.endpoint)).data
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${endpoint.bearer}`
+      }
+    }
+
     let content = null
-
-    const bearer = endpoint.secret.find(
-      (x) => x.key === 'bearer' || x.key === 'authToken'
-    )
-
-    if (req.query['name']) {
-      let url = new URL(
+    if (req.query.name) {
+      const url = new URL(
         uriHelpers.concatUrl([endpoint.target, req.params.name, 'resource'])
       )
       Object.keys(req.query).forEach((key) =>
         url.searchParams.append(key, req.query[key])
       )
-      content = (
-        await axios.get(url.toString(), {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${bearer.val}`
-          }
-        })
-      ).data
+      content = (await axios.get(url.toString(), headers)).data
     } else {
       const tree = await axios.get(
-        uriHelpers.concatUrl([
-          endpoint.target,
-          req.params.name,
-          'resource-tree'
-        ]),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${bearer.val}`
-          }
-        }
+        uriHelpers.concatUrl([endpoint.target, req.params.name]),
+        headers
       )
       content = argocdHelpers.nodeEdges(tree.data, req.params.name)
     }
 
+    if (!content) {
+      res.status(500).json({ message: 'No content' })
+    }
+
     return res.status(200).json(content)
   } catch (error) {
-    console.log(error)
     next(error)
   }
 })
