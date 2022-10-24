26afc61ed98ce71ac16d22b69c1ee679bc74faff
mauro sala <mauro.sala@gmail.com>
2022-10-24 17:45:09 +0200

    fix(read): logger debug

--- controllers/read.argocd.controller.js
+++ controllers/read.argocd.controller.js
@@ -4,10 +4,13 @@ const axios = require('axios')
 const uriHelpers = require('../service-library/helpers/uri.helpers')
 const argocdHelpers = require('../helpers/argocd.helpers')
 const secretHelpers = require('../service-library/helpers/secret.helpers')
+const logger = require('../service-library/helpers/logger.helpers')
 
 router.get('/:endpoint/:name', async (req, res, next) => {
   try {
     const endpoint = (await secretHelpers.getEndpoint(req.params.endpoint)).data
+
+    logger.debug(endpoint)
 
     const headers = {
       headers: {
@@ -15,6 +18,8 @@ router.get('/:endpoint/:name', async (req, res, next) => {
         Authorization: `Bearer ${endpoint.bearer}`
       }
     }
+
+    logger.debug(headers)
 
     let content = null
     if (req.query.name) {
